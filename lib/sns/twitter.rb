05d0363f88ed6ed7fca24e252bb1b2fb62308a07
akihia.sengoku <akihisa1788@yahoo.co.jp>
2018-01-18 09:31:12 +0900

    [fix] tweet text

--- lib/sns/twitter.rb
+++ lib/sns/twitter.rb
@@ -7,13 +7,11 @@ module Sns::Twitter extend self
 
     # 特定ユーザのtimelineを件数(1件)指定して取得
     @client.user_timeline(@username, { count: 1 } ).each do |timeline|
-      text = @client.status(timeline.id).text.gsub(/(\s)/,"")
-      puts "#{text} #{Time.now}"
-      if last_tweet = Tweet.all.last
-        if text != last_tweet.body
-          post_to_googlehome(text)
-        end
-      else
+      tweet = @client.status(timeline.id).text
+      puts "#{tweet} #{Time.now}"
+      text = modify_text(tweet)
+      puts "#{text}"
+      if check_tweet(text)
         post_to_googlehome(text)
       end
     end
@@ -27,6 +25,28 @@ module Sns::Twitter extend self
         config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
         config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
       end
+    end
+
+    def modify_text(text)
+      new_text = text.gsub(/(\s)/,"")
+      urls = URI.extract(new_text)
+      urls.each do |url|
+        new_text.slice!(url)
+      end
+      return new_text
+    end
+
+    def check_tweet(text)
+      if text[0] != "R"
+        if last_tweet = Tweet.all.last
+          if text != last_tweet.body
+            return true
+          end
+        else
+          return true
+        end
+      end
+      return false
     end
 
     def post_to_googlehome(text)
